10e7ce769aa61e3683bd7f01c3f61f9b74c16235
adrian salona <Adrian>
2023-04-22 17:28:42 +0200

    Feat: the structure for displaying the products in the home view is created

--- techVision/src/components/Card/Card.jsx
+++ techVision/src/components/Card/Card.jsx
@@ -5,11 +5,10 @@ import './Card.css'
 function CardComponent({ image, showButton, title, text }) {
   return (
     <div className='cont-treatment-1'>
+        <div className='title-treatment-1'>{title}</div>
         <div className="cont-all">
             <img className='img-logo' src={image} alt=""/>
-            <div className='title-treatment-1'>{title}</div>
-            <p className='text-treatment-1'>{text}</p>
-            {showButton && <Button className='appointment-btn'>Saber más <i class="bi bi-chevron-right arrow"></i></Button>}
+            {showButton && <Button className='appointment-btn'>ADD TO CART</Button>}
         </div>
     </div>
   );
--- techVision/src/layout/home/Home.jsx
+++ techVision/src/layout/home/Home.jsx
@@ -3,6 +3,8 @@ import { NavBar } from '../../components/Navbar/NavBar';
 import './Home.css';
 import productPrincipal from '../../assets/iphone-13-pro-max-plata-5-1-500.png';
 import productMac from '../../assets/mac-prp-1.png'
+import CardComponent from '../../components/Card/Card';
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 
@@ -35,7 +37,68 @@ export const Home = () => {
                 </div>
             </div>
         </div>
-        
+        <Container className='container-products'>
+            <Row className='flex-row d-flex product-home '>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                />                  
+                </Col>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container card-final">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+                <Col md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardComponent
+                    image={productMac}
+                    showButton={true} 
+                    title= "NEW"
+                /> 
+                </Col>
+            </Row>
+        </Container>
         </>
     )
 };
