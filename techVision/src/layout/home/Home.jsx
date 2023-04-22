1248a54e67a1285ad25cc19eb2557406c4106c26
adrian salona <Adrian>
2023-04-22 13:15:04 +0200

    Feat: main div of the home view is performed prior to the categories and product view.

--- techVision/src/layout/home/Home.jsx
+++ techVision/src/layout/home/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { NavBar } from '../../components/Navbar/NavBar';
 import './Home.css';
+import productPrincipal from '../../assets/iphone-13-pro-max-plata-5-1-500.png';
+import productMac from '../../assets/mac-prp-1.png'
 
 
 
 export const Home = () => {
     return (
+        <>
         <NavBar className='navigator'/>
+        <div className='container-product'>
+            <div className='visual-product'>
+                <div className='text-product'>
+                    <p className='best'>THE BEST TECHNOLOGY</p>
+                    <p className='offers'>Techv offers you the best prices and conditions as well as an extensive catalog in our store.</p>
+                </div>
+                <div className='img-product'>
+                    <img src={productMac} alt="" />
+                    <img className='img-movil-prp' src={productPrincipal} alt="" />
+                </div>
+            </div>
+        </div>
         
+        </>
     )
 };
