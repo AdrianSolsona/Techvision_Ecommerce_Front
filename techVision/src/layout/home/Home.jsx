d68ab17b38fa99cc93db94ddaca94601ac74da9d
Adrian Solsona <solsona1008@gmail.com>
2023-05-06 19:28:18 +0200

    Chore: home view is modified with other images and responsive aspects are included

--- techVision/src/layout/home/Home.jsx
+++ techVision/src/layout/home/Home.jsx
@@ -4,11 +4,16 @@ import './Home.css';
 import productPrincipal from '../../assets/iphone-13-pro-max-plata-5-1-500.png';
 import productMac from '../../assets/mac-prp-1.png'
 import CardComponent from '../../components/Card/Card';
+import CardHome from '../../components/CardHome/CardHome';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Footer } from '../../components/Footer/Footer';
 import { Navigator } from '../../components/Navigator/Navigator';
 import { useNavigate } from 'react-router-dom';
-
+import  productAirPods  from '../../assets/airpods-max-removebg-preview.png'
+import  watchUltra  from '../../assets/apple-watch-removebg-preview.png'
+import  appleTv  from '../../assets/apple-tv-1-removebg-preview.png'
+import  ipadPro  from '../../assets/ipad-pro-1-removebg-preview.png'
+import macBook from '../../assets/macbook-air-removebg-preview.png'
 
 export const Home = () => {
     return (
@@ -35,144 +40,71 @@ export const Home = () => {
         <Container className='container-products'>
             <Row className='flex-row d-flex product-home '>
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
+                <CardHome
+                    image={ipadPro}
                     showButton={true} 
                     title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
+                    productName="IPAD PRO'"
+                    price="579"
                 />                  
                 </Col>
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
+                <CardHome
+                    image={productAirPods}
                     showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
+                    title= "AVAIABLE"
+                    productName="AIRPODS PRO"
+                    price="299"
                 /> 
                 </Col>
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
+                <CardHome
+                    image={watchUltra}
                     showButton={true} 
                     title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
+                    productName="APPLE WATCH ULTRA"
+                    price="400"
                 /> 
                 </Col>
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
+                <CardHome
+                    image={appleTv}
                     showButton={true} 
                     title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
+                    productName="APPLE TV 4K"
+                    price="1700"
                 /> 
                 </Col>
             
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container card-final">
-                <CardComponent
+                <CardHome
                     image={productMac}
                     showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
+                    title= "AVAIABLE"
+                    productName="IMAC 24'"
+                    price="1500"
                 /> 
                 </Col>
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
+                <CardHome
+                    image={macBook}
+                    showButton={true} 
+                    title= "NEW"
+                    productName="MACBOOK AIR"
+                    price="1250"
+                />
+                </Col>
+                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
+                <CardHome
+                    image={productPrincipal}
                     showButton={true} 
                     title= "NEW"
                     productName="IPHONE PRO MAX"
-                    price="1100"
+                    price="1200"
                 /> 
                 </Col>
                 <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container card-final">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-            
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container card-final">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
-                    image={productMac}
-                    showButton={true} 
-                    title= "NEW"
-                    productName="IPHONE PRO MAX"
-                    price="1100"
-                /> 
-                </Col>
-                <Col xs={12} md={6} lg={6} xl={4} xxl={3} className="card-container">
-                <CardComponent
+                <CardHome
                     image={productMac}
                     showButton={true} 
                     title= "NEW"
