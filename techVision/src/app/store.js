ff9430820bf8b938ca0d4e0e7da36c4142f25ec1
Adrian Solsona <solsona1008@gmail.com>
2023-05-06 19:18:47 +0200

    Feat: the detail slice is added to the store

--- techVision/src/app/store.js
+++ techVision/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../layout/userSlice';
 import addressSlice from '../layout/userSlice';
-import detailSlice from '../layout/userSlice';
+import detailSlice from '../layout/detailSlice';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
