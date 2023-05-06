5c6b96e963545518b34812358d86fc498cbee3b8
Adrian Solsona <solsona1008@gmail.com>
2023-05-06 19:29:30 +0200

    Feat: letter is made to display product data in home and shop

--- techVision/src/components/Card/Card.jsx
+++ techVision/src/components/Card/Card.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Button } from 'react-bootstrap';
 import './Card.css'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 
 function CardComponent({ image, showButton, title, text, productName, price }) {
+
   return (
     <div className='cont-treatment-1'>
         <div className='title-treatment-1'>{title}</div>
         <div className="cont-all">
             <img className='img-logo' src={image} alt=""/>
-            {showButton && <Button className='appointment-btn'>ADD TO CART</Button>}
+            {showButton && <Button className='appointment-btn'>VIEW IN DETAIL</Button>}
         </div>
         <div className='title-product-sh'>{productName}</div>
         <div className='title-product-sh-2'><div>$</div>{price}</div>
